6ea6d1b344b1b336380b8875e9a90edafbfa5c6d
opiyawan <piyawan6752@gmail.com>
2017-11-24 10:14:13 +0700

    Test variable

---
+++ src/ooplab2/TestVariable.java
@@ -0,0 +1,35 @@
+package ooplab2;
+
+public class TestVariable {
+    public static void main(String[] args) {
+//        Integer
+                int i;
+                i = 10;
+                System.out.print("i ="+i);
+                int j = 20;
+                System.out.println("j ="+j);
+                System.out.println("i+j="+(i+j));
+                // Double
+        double v = 15.15;
+        System.out.print(v);
+        System.out.print(v *10);
+        char c = 'T';
+        String s = "Hello World";
+        System.out.println(s);
+        String name = "I'm PRIM";
+        System.out.println(name);
+        String msg = s + " " + name;
+        System.out.println(msg);
+        String msg2 = s + " " + name;
+        System.out.println(msg2);
+        System.out.println(msg2);
+        System.out.println(msg.length());
+        System.out.println(msg.toUpperCase());
+        System.out.println(msg.toLowerCase());
+//        boolean
+        boolean b = true;
+        boolean b1 = false;
+        System.out.print(b);
+        System.out.print(b1);
+    }
+}//class
