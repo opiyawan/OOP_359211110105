f11ca09c788a52e2384e4b41a26400b5c7d91396
opiyawan <piyawan 6752@gmail.com>
2017-12-22 11:15:49 +0700

    myArray2D

---
+++ src/ooplab5/Exeercise.java
@@ -0,0 +1,7 @@
+package ooplab5;
+
+public class Exeercise {
+    public static void main (String[] args){
+        
+    }
+}
---
+++ src/ooplab5/myArrayList.java
@@ -0,0 +1,15 @@
+package ooplab5;
+
+import java.util.ArrayList;
+
+public class myArrayList {
+    public static void main (String[] args){
+        //Arraylist
+        ArrayList list = new ArrayList();
+        String name ("NAN")
+        list.add("KIK")
+        String name = "SAM";
+        list.add("PRIM");
+        System.out.print(list);
+    }
+}
