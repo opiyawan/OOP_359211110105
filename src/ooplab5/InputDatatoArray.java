0211866d1e90c2484859752e607720f98815ccdb
opiyawan <piyawan 6752@gmail.com>
2017-12-22 09:59:07 +0700

    InputDatatoArray.java

--- src/ooplab5/InputDatatoArray.java
+++ src/ooplab5/InputDatatoArray.java
@@ -10,7 +10,21 @@ public class InputDatatoArray {
         System.out.println(number.length);
         //for
         number = inputData(number);
+        showData(number);
+        showDataEnchance(number);
     }//main
+
+    private static void showDataEnchance(int[] number) {
+        System.out.println("Data in Array: ");
+        for (int val:number)
+            System.out.print(val+" ");
+    }
+
+    private static void showData(int[] number) {
+        System.out.println("Data in Array: ");
+        for (int i=0;i<number.length;i++)
+            System.out.print(number[i]+"");
+    }
 
     private static int[] inputData(int[] number) {
         Scanner scanner = new Scanner(System.in);
