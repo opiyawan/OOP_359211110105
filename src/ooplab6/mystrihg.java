66d3484b8120278d1fc014fcf4007c759d8314f8
opiyawan <piyawan6752@gmail.com>
2017-12-29 09:17:47 +0700

    mystrihg.java

---
+++ src/ooplab6/mystrihg.java
@@ -0,0 +1,43 @@
+package ooplab6;
+
+public class mystrihg {
+    public static void main(String[] args) {
+        char[] c = {'H','e','l','l','o'};
+        String str1 = new String(c);
+        System.out.println(str1);
+
+        String str2 = "piyawan Chiairi";
+        System.out.println(str2);
+
+
+
+        String str3 = new String("RMUTSV");
+        System.out.println(str3);
+        //concatenation String
+        //type (+)
+        String str4 = str1 +" "+ str2;
+        System.out.print(str4);
+        //type (concat() method)
+        String str5 = str1.concat(" "+str3);
+        System.out.println(str5);
+
+        //String size
+        int length = str4.length();
+        System.out.println(length);
+        System.out.println(str5.length());
+
+//        string methog
+        System.out.println(str5.toLowerCase());
+        System.out.println(str5.toLowerCase());
+        System.out.println(str5.replace('l','s'));
+
+        //trim()
+        String str6 = " Hello ";
+        System.out.println(str6.length());
+        System.out.println(""+str6+"");
+        System.out.println(""+str6.trim()+"");
+
+
+
+    }//main
+}//class
