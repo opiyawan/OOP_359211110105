4a317a765e9f6c87bb52ef6eb0cbb42cbeb540b4
opiyawan <piyawan6752@gmail.com>
2018-01-21 17:36:48 -0800

    yeartest.jiava

---
+++ src/Test/yeartest.java
@@ -0,0 +1,32 @@
+package Test;
+
+import java.util.Scanner;
+
+public class yeartest {
+    public static void main(String[] args) {
+
+        Scanner inpul = new Scanner(System.in);
+
+            int yearBorn = inpul.nextInt();
+            int yearNow = 2557;
+
+            System.out.println("Enter Year1:2532 ");
+            System.out.println("Age1 : " + (yearNow - 2532));
+
+
+            System.out.println("Enter Year2:2510");
+            System.out.println("Age2 : " + (yearNow - 2510));
+
+
+            System.out.println("Enter Year3:2528");
+            System.out.println("Age3 : " + (yearNow - 2528));
+
+            System.out.println("Thank you.");
+
+
+
+
+    }//
+
+
+}// class
